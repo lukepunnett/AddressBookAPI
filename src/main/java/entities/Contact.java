f8f03c2251f04ed4b63af8521dfe7093a90b7e0e
Luke Punnett <31255258+LukeJDPunnett@users.noreply.github.com>
2019-06-24 19:30:06 +0100

    Update Contact Class to be be annotated
    Added spring entity annotation, toString method and also added some validation to entity fields.

--- src/main/java/entities/Contact.java
+++ src/main/java/entities/Contact.java
@@ -2,12 +2,28 @@ package entities;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Pattern;
+
+@Entity
 public class Contact {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
+	
 	private String firstName;
 	private String secondName;
+	
+	@PastOrPresent
 	private Date dateOfBirth;
+	
+	@Pattern(regexp="^[_A-Za-z0-9-\\\\+]+(\\\\.[_A-Za-z0-9-]+)*\r\n" + 
+			"      @[A-Za-z0-9-]+(\\\\.[A-Za-z0-9]+)*(\\\\.[A-Za-z]{2,})$;")
 	private String emailAddress;
 	
 	public Contact(){
@@ -62,9 +78,12 @@ public class Contact {
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
 	}
-	
-	
-	
+
+	@Override
+	public String toString() {
+		return "Contact [id=" + id + ", firstName=" + firstName + ", secondName=" + secondName + ", dateOfBirth="
+				+ dateOfBirth + ", emailAddress=" + emailAddress + "]";
+	}
 	
 	
 }
